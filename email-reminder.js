de2c1ec29925dae552b0f37638ab798da6ee000b
p.demeshchik <p.demeshchik@datarockets.com>
2015-09-07 17:17:26 +0300

    Basic functions

--- email-reminder.js
+++ email-reminder.js
@@ -1,24 +1,85 @@
+Reminders = new Mongo.Collection('reminders');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.reminderList.helpers({
+    reminders: function () {
+      return Reminders.find();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.reminderList.events({
+    'click .add-reminder-btn': function () {
+      $('.new-reminder-form').show();
+    },
+
+    'click .remove-btn': function () {
+      Reminders.remove(this._id);
+    },
+
+    'click .disable-btn': function () {
+      Reminders.update(this._id, {$set: {active: !this.active}});
+    }
+
+
+  });
+
+  Template.newReminderForm.events({
+    'submit': function (event) {
+      var active = event.target.active.value == "on";
+      var name = event.target.name.value;
+      var email = event.target.email.value;
+      var to = event.target.to.value;
+      var startTime = event.target.startTime.value;
+      var startDate = event.target.startDate.value;
+      var periodicity = event.target.periodicity.value;
+
+      Reminders.insert({
+        active: active,
+        name: name, 
+        email: email, 
+        to: to, 
+        startTime: startTime, 
+        startDate: startDate, 
+        periodicity: periodicity
+      });
+
+      $('.new-reminder-form-input').val("").removeAttr("checked");
+
+      return false;
     }
   });
-  alert('test');
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    SyncedCron.add({
+      name: 'Send email scheduled task',
+      schedule: function(parser) {
+      return parser.text('every 10 seconds');
+      },
+      job: function() {
+        console.log("Running!!!");
+        Meteor.call('sendEmail', 'p.demeshchik@datarockets.com', 'p.demeshchik@datarockets.com', 'test meteor', 'testing');
+      }
+    });
+    SyncedCron.start();
+  });
+
+  Meteor.methods({
+    sendEmail: function (to, from, subject, text) {
+      check([to, from, subject, text], [String]);
+
+      // Let other method calls from the same client start running,
+      // without waiting for the email sending to complete.
+      this.unblock();
+
+      Email.send({
+        to: to,
+        from: from,
+        subject: subject,
+        text: text
+      });
+    }
   });
 }
